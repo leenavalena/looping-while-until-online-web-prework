84dfcd0105df80572e237bf53e633f870b154837
Valena Del Valle <valenadelvalle4@gmail.com>
2019-07-01 23:34:41 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,10 +1,7 @@
 def using_until
   levitation_force = 6
-counter = 0
-loop do
-  counter = counter + 1
-  until counter == 10
-    puts "Wingardium Leviosa"
-    counter += 1
+      until levitation_force == 10
+      puts "Wingardium Leviosa"
+    levitation_force += 1
   end
 end
--- lib/while.rb
+++ lib/while.rb
@@ -1,10 +1,7 @@
-
 def using_while
-  levitation_force = 6
-counter = 10
-loop do
-  counter = counter - 1
-  until counter < 10
+levitation_force = 6
+ while levitation_force < 10 
     puts "Wingardium Leviosa"
+      levitation_force += 1
   end
 end
